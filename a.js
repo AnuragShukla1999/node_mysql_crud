f93c3b50d5d74bc8b7890596eb5d20f51c8b827c
Anurag Shukla <shuklaanurag310@gmail.com>
2024-09-05 18:44:09 +0530

    .

--- a.js
+++ a.js
@@ -367,3 +367,47 @@
 // const error = new Error("hello");
 
 // console.log(error);
+
+
+
+
+
+
+
+
+
+
+/// max and min in array
+
+// function findMinMax(arr) {
+//     arr.sort((a, b) => a - b);
+//     return { min: arr[0], max: arr[arr.length - 1] };
+// }
+
+// const arr = [1, 423, 6, 46, 34, 23, 13, 53, 4];
+// const { min, max } = findMinMax(arr);
+// console.log(`min-${min} max-${max}`); // Output: min-1 max-423
+
+
+
+
+
+function findMinMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return { min, max };
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const { min, max } = findMinMax(arr);
+console.log(`min-${min} max-${max}`);
